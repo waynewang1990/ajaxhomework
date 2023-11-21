bfebe2db8849ccb0ec57e15b9a57cbb75f4cd2b6
wayne <waynewang1990@hotmail.com>
2023-11-21 16:32:22 +0800

    1

---
+++ ajaxhomework/Controllers/HW1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace ajaxhomework.Controllers
+{
+    public class HW1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HW1()
+        {
+           
+            return View();
+        }
+    }
+}
