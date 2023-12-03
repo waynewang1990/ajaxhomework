d922732412eebf02a45d61a8abf6ef9159f53f38
waynewang <waynewang1990@hotmail.com>
2023-12-04 03:09:21 +0800

    .

---
+++ ajaxhomework/Controllers/APIController.cs
@@ -0,0 +1,18 @@
+﻿using ajaxhomework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ajaxhomework.Controllers
+{
+    public class APIController : Controller
+    {
+        private readonly IWebHostEnvironment _host;
+        private readonly DemoContext _context;
+        public APIController(IWebHostEnvironment host, DemoContext context)
+        {
+            _host = host;
+            _context = context;
+        }
+
+       
+    }
+}
---
+++ ajaxhomework/Controllers/HWController.cs
@@ -0,0 +1,68 @@
+﻿using ajaxhomework.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.EntityFrameworkCore;
+
+namespace ajaxhomework.Controllers
+{
+    public class HWController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HW1()
+        {
+           
+            return View();
+        }
+
+        public IActionResult HW2()
+        {
+
+            return View();
+        }
+
+        public string CheckAccount(Member m)
+        {
+            DemoContext context = new DemoContext();
+            
+            string c = "該姓名可以使用";
+            if (context.Members.Any(p=>p.Name== m.Name))
+            {
+                c = "該用戶已註冊";
+            }
+
+            return c;
+        }
+
+        public IActionResult HW3() 
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            DemoContext context = new DemoContext();
+            var cities = context.Addresses.Select(c => c.City).Distinct();
+            return Json(cities);
+        }
+
+        //根據城市名稱讀取不會重複的鄉鎮區名稱
+        public IActionResult Districts(string city)
+        {
+            DemoContext context = new DemoContext();
+            var districts = context.Addresses.Where(a => a.City == city).Select(a => a.SiteId).Distinct();
+            return Json(districts);
+        }
+
+        //根據鄉鎮區讀取路的名稱
+        public IActionResult Roads(string siteId)
+        {
+            DemoContext context = new DemoContext();
+            var roads = context.Addresses.Where(a => a.SiteId == siteId).Select(a => a.Road);
+            return Json(roads);
+        }
+    }
+}
